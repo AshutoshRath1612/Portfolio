b845d8af4d0c8e3a53fe2c6f452e9118e43ac35e
Ashutosh Rath <ashutoshrath1612@gmail.com>
2026-09-20 22:06:35 +0530

    refactor: update robots function syntax for consistency and clarity

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,15 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 import { getPortfolio } from "./lib/content";
 
-export const robots = (): MetadataRoute.Robots => {
-    const {seo} = getPortfolio();
+export default function robots(): MetadataRoute.Robots {
+  const { seo } = getPortfolio();
 
-    return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-        },
-        sitemap: `${seo.url}/sitemap.xml`,
-        host: seo.url,
-    }
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${seo.url}/sitemap.xml`,
+    host: seo.url,
+  };
 }
